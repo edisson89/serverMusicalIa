eb94b0e93e3a46143993a27a35e4838aa088d925
edisson89 <edisson.12@hotmail.com>
2024-05-01 20:22:47 -0500

    cambios funcion nota

--- src/controllers/addNotaControllers.js
+++ src/controllers/addNotaControllers.js
@@ -1,7 +1,10 @@
 const {
   DynamoDBClient,
+  GetItemCommand,
+  PutItemCommand,
   
 } = require("@aws-sdk/client-dynamodb");
+
 const {
   DynamoDBDocumentClient,
   GetCommand,
@@ -11,52 +14,46 @@ const {
 
 async function addNotaControllers(titulo, descripcion) {
   try {
+    
     require("dotenv").config();
     const {
       v4: uuidv4
     } = require("uuid");
     const USERS_TABLE = process.env.USERS_TABLE;
     const userId = uuidv4();
-
+    
     const client = new DynamoDBClient();
     const dynamoDbClient = DynamoDBDocumentClient.from(client);
-
+    
     const createAT = new Date();
     const formattedDate = createAT.toISOString().split("T")[0]; // "2024-04-22"
-
+    
     const nota = {
       userId,
       formattedDate,
       titulo,
       descripcion,
     };
-
+    
     const params = {
       TableName: USERS_TABLE,
       Item: nota,
     };
-
+    
     // Verificar si la nota ya existe
-    const existingNote = await dynamoDbClient.send(new GetCommand({
+     await dynamoDbClient.send(new GetCommand ({
       TableName: USERS_TABLE,
       Key: {
-        "titulo": titulo,
+        "userId": userId,
         
       }
     }));
-
-    // Si la nota ya existe, lanzar un error
-    if (!existingNote || !existingNote.Item) {
+         
+      
       await dynamoDbClient.send(new PutCommand(params));
-
+      
       return nota;
-
-
-    } else {
-
-      console.log('ingreso error', existingNote)
-      throw new Error("La nota ya existe en la base de datos");
-    }
+    
 
 
   } catch (error) {
--- src/handlers/addNota.js
+++ src/handlers/addNota.js
@@ -8,10 +8,12 @@ const addNota = async (req, res) => {
         } = req.body
 
         const result = await addNotaControllers(titulo, descripcion);
-        
+
         res.status(200).json({result})
     } catch (error) {
-        res.status(404).json({error: error.message});
+        res.status(404).json({
+            error: error.message
+        });
     }
 
 }
