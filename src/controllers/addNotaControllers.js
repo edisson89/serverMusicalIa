be60e069b104cad79a2633945685695537661111
edisson89 <edisson.12@hotmail.com>
2024-05-22 17:07:36 -0500

    ultimo msj

--- src/controllers/addNotaControllers.js
+++ src/controllers/addNotaControllers.js
@@ -1,20 +1,12 @@
 const {
   DynamoDBClient,
   GetItemCommand,
-  PutItemCommand,
-  
+  PutItemCommand
 } = require("@aws-sdk/client-dynamodb");
+const { GetCommand, PutCommand } = require("@aws-sdk/lib-dynamodb");
 
-const {
-  DynamoDBDocumentClient,
-  GetCommand,
-  PutCommand,
-
-} = require("@aws-sdk/lib-dynamodb");
-
-async function addNotaControllers(titulo, descripcion) {
+async function addNotaControllers (titulo, descripcion){
   try {
-    
     require("dotenv").config();
     const {
       v4: uuidv4
@@ -23,38 +15,49 @@ async function addNotaControllers(titulo, descripcion) {
     const userId = uuidv4();
     
     const client = new DynamoDBClient();
-    const dynamoDbClient = DynamoDBDocumentClient.from(client);
     
     const createAT = new Date();
     const formattedDate = createAT.toISOString().split("T")[0]; // "2024-04-22"
     
     const nota = {
       userId,
-      formattedDate,
       titulo,
       descripcion,
+      formattedDate,
     };
+    
     
     const params = {
       TableName: USERS_TABLE,
-      Item: nota,
-    };
-    
-    // Verificar si la nota ya existe
-     await dynamoDbClient.send(new GetCommand ({
-      TableName: USERS_TABLE,
       Key: {
-        "userId": userId,
-        
-      }
-    }));
-         
-      
-      await dynamoDbClient.send(new PutCommand(params));
-      
-      return nota;
+        userId: userId,
+      },
+    };
+    // Verificar si la nota ya existe
+    
+    const VerificarItem = await client.send(new GetCommand(params));
+    console.log('in',VerificarItem)
+    const existItem = VerificarItem.Item
     
 
+    if(existItem){
+      const newItem = VerificarItem.Item.userId !== undefined
+      if (newItem) {
+      throw new Error('Item ya existe')
+
+    } else {
+      const paramsNote = {
+        TableName: users-table-dev,
+        Item: nota,
+      };
+      console.log(paramsNote)
+      const result = await client.send(new PutCommand(paramsNote));
+      const newResult = result.Attributes
+      return newResult
+    }
+    }
+    console.log(newItem)
+ 
 
   } catch (error) {
 
